02edc12390624ace88b6f282d19b7516614a1637
MindedParsley <jonathan@koster.cx>
2026-09-09 20:32:41 +0100

    added functionality for opening a window + game/base classes

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,8 +1,19 @@
-
-
+#include <DX3D/All.h>
 
 int main() {
-
-
+	try {
+		dx3d::Game game{};
+		game.run();
+	}
+	catch (const std::runtime_error& e) {
+		return EXIT_FAILURE;
+	}
+	catch (const std::exception& e) {
+		return EXIT_FAILURE;
+	}
+	catch (...) {
+		return EXIT_FAILURE;
+	}
 	return 0;
+
 }
