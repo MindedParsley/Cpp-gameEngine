8a436bd0f7805b0d2f4907d7b5c40582c9afb88e
MindedParsley <jonathan@koster.cx>
2026-09-09 21:07:18 +0100

    fixed git ignore ignoring some needed files

---
+++ DX3D/Source/DX3D/Window/Win32/Win32Window.cpp
@@ -0,0 +1,57 @@
+#include <DX3D/Window/Window.h>
+#include <Windows.h>
+#include <stdexcept>
+
+
+static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+dx3d::Window::Window(): Base()
+{
+	auto registerWindowClassFunction = []()
+		{
+			WNDCLASSEX wc{};
+			wc.cbSize = sizeof(WNDCLASSEX);
+			wc.lpszClassName = L"DX3DWindow";
+			wc.lpfnWndProc = &WindowProc;
+			return RegisterClassEx(&wc);
+		};
+
+
+
+	static const auto windowClassId = std::invoke(registerWindowClassFunction);
+
+
+	if (!windowClassId)
+	{
+		throw std::runtime_error("Failed to register window class");
+	}
+
+	RECT rc = { 0, 0, 1280, 720 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW | WS_CAPTION | WS_SYSMENU, FALSE);
+
+	m_handle = CreateWindowEx(NULL, MAKEINTATOM(windowClassId), L"JK engine", WS_OVERLAPPEDWINDOW | WS_CAPTION | WS_SYSMENU, CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, NULL, NULL);
+
+	if (!m_handle)
+	{
+		throw std::runtime_error("Failed to create window");
+	}
+
+	ShowWindow(static_cast<HWND>(m_handle), SW_SHOW);
+
+}
+
+dx3d::Window::~Window()
+{
+	DestroyWindow(static_cast<HWND>(m_handle));
+}
